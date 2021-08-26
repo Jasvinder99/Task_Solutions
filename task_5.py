0f30c404da0f49d8c98b01678b291d8a66999042
Jasvinder kaur Deve <75005221+Jasvinder99@users.noreply.github.com>
2021-08-26 15:29:41 +0530

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -5,12 +5,15 @@
 
 import bs4
 import requests
+from typing import Optional
+
 
 #----------------------------------------------------------------------------------
 
-get_request = requests.get("https://news.ycombinator.com/")
-var_soup = bs4.BeautifulSoup(get_request.text, 'lxml')
-news_title = var_soup.select(".storylink")      #storylink is class from a tag to get all news titles
+get_request: Optional = requests.get("https://news.ycombinator.com/")
+var_soup: Optional = bs4.BeautifulSoup(get_request.text, 'lxml')
+#storylink is class from a tag to get all news titles
+news_title: Optional[classmethod] = var_soup.select(".storylink")
 
 for i in var_soup.select('.storylink'):
     print(i.text)
